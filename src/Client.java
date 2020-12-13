ef4648f8b303183171c64942266182e2f42743a8
horobito <odkfu1@gmail.com>
2020-12-14 02:14:22 +0900

    1. 만든 것  1) 클라에서 입력한 것이 서버를 거쳐 다시 돌아오는 것  2) 조건 만족할 시 소켓 닫는 것

--- src/Client.java
+++ src/Client.java
@@ -1,31 +1,64 @@
 import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 
 public class Client {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Socket socket = null;
 
         OutputStream outputStream = null;
 
+        DataOutputStream dataOutputStream = null;;
+
+        // 소켓의 입력스트림을 얻는 과정
+        InputStream inputStream = null;;
+        DataInputStream dataInputStream = null;;
+        Scanner sc = new Scanner(System.in);
 
         try {
             // 소켓을 생성 및 연결 요청, 이때 연결할 서버의 ip와 포트번호 입력
             socket = new Socket("localhost", 8080);
+            System.out.println("클라이언트 접속 성공");
 
-            OutputStream outputStream1 = socket.getOutputStream();
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+            outputStream = socket.getOutputStream();
+             dataOutputStream = new DataOutputStream(outputStream);
 
             // 소켓의 입력스트림을 얻는 과정
-            InputStream inputStream = socket.getInputStream();
-            DataInputStream dataInputStream = new DataInputStream(inputStream);
+            inputStream = socket.getInputStream();
+            dataInputStream = new DataInputStream(inputStream);
 
-            System.out.println(dataInputStream.readUTF());
+
+
+            while (true){
+                String inputByClient = sc.nextLine();
+
+                dataOutputStream.writeUTF(inputByClient);
+                dataOutputStream.flush();
+
+
+
+                System.out.println("서버 응답 : " + dataInputStream.readUTF());
+
+                if(inputByClient.equals("closeAndFinish")){
+                    System.out.println("통신 종료 시작");
+                    break;
+                }
+
+
+            }
 
 
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if(dataOutputStream !=null){ dataOutputStream.close();}
+            if(outputStream !=null){outputStream.close();}
+            if(dataInputStream !=null){dataInputStream.close();}
+            if (inputStream !=null){inputStream.close();}
+            socket.close();
+            System.out.println("통신 종료");
         }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,38 +1,73 @@
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         ServerSocket serverSocket = null;
         Socket cliSocket = null;
 
+        InputStream inputStream = null;
+        DataInputStream dataInputStream = null;
+
+        OutputStream outputStream = null;
+        DataOutputStream dataOutputStream = null;
+
+
+
         try{
             // 서버 소켓 생성 및, 포트(8080)와 결합
             serverSocket = new ServerSocket(8080);
+            System.out.println("클라이언트 접속 대기중 ");
             
             // 요청이 들어올 때까지 대기했다가,
             // 클라 요청 시 클라 소켓과 통신할 소켓 반환
             cliSocket = serverSocket.accept();
 
 
+            inputStream = cliSocket.getInputStream();
+            dataInputStream = new DataInputStream(inputStream);
+
+            // 소켓의 출력 스트림 얻는 것
+            outputStream = cliSocket.getOutputStream();
+            dataOutputStream = new DataOutputStream(outputStream);
+
+
+
+            while (true){
+                try {
+
+
+                    String inputData = dataInputStream.readUTF();
+
+                    if(inputData.equals("closeAndFinish")){
+                        dataOutputStream.writeUTF("Your inputString is " + inputData + "\n"
+                                + "and my answer is 'ByeBye'");
+                        break;
+                    }
+                    dataOutputStream.writeUTF("Your inputString is " + inputData + "\n"
+                            + "and my answer is 'hello'");
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        while (true){
-            try {
-                // 소켓의 출력 스트림 얻는 것
-                OutputStream outputStream = cliSocket.getOutputStream();
-                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        }finally {
+            if(dataOutputStream !=null){ dataOutputStream.close();}
+            if(outputStream !=null){outputStream.close();}
+            if(dataInputStream !=null){dataInputStream.close();}
+            if (inputStream !=null){inputStream.close();}
+            cliSocket.close();
+            serverSocket.close();
 
-                dataOutputStream.writeUTF("hello");
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
+
+
     }
 }
